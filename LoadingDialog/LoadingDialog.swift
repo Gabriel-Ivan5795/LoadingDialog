9f44e0395268f068ab16ee99f3786ee5d6b95878
Ivan Gabriel <gabriel.ivan@roweb.ro>
2020-03-20 10:49:19 +0200

    solve bugs at show hidden loader

--- LoadingDialog/LoadingDialog.swift
+++ LoadingDialog/LoadingDialog.swift
@@ -11,24 +11,21 @@ import Foundation
 public class LoadingDialog {
     
     static var loaderView: LoadingDialogView!
-    var originalKeyWindow: UIWindow!
-    var progressWindow: UIWindow!
 
-    class func sharedView(context: UIViewController) -> LoadingDialogView? {
-
+    class func addView(context: UIViewController) -> LoadingDialogView? {
         if (self.loaderView == nil) {
             self.loaderView = LoadingDialogView.init(frame: CGRect.init(x: (GetSizes.getTotalWidth() - 200) / 2, y: (GetSizes.getTotalHeight() - 200) / 2, width: 200, height: 200))
             context.view.addSubview(self.loaderView)
         } else {
             self.loaderView.removeFromSuperview()
-            self.loaderView = nil
+            self.loaderView = LoadingDialogView.init(frame: CGRect.init(x: (GetSizes.getTotalWidth() - 200) / 2, y: (GetSizes.getTotalHeight() - 200) / 2, width: 200, height: 200))
+            context.view.addSubview(self.loaderView)
         }
         
         return loaderView
     }
     
-    class func sharedLoaderView(context: UIViewController, withColor color: UIColor, andFont font: UIFont) -> LoadingDialogView? {
-
+    class func addView(context: UIViewController, withColor color: UIColor, andFont font: UIFont) -> LoadingDialogView {
         if (self.loaderView == nil) {
             self.loaderView = LoadingDialogView.init(frame: CGRect.init(x: (GetSizes.getTotalWidth() - 200) / 2, y: (GetSizes.getTotalHeight() - 200) / 2, width: 200, height: 200))
             context.view.addSubview(self.loaderView)
@@ -36,22 +33,34 @@ public class LoadingDialog {
             self.loaderView.setBackgroundColor(_color: color)
         } else {
             self.loaderView.removeFromSuperview()
-            self.loaderView = nil
+            self.loaderView = LoadingDialogView.init(frame: CGRect.init(x: (GetSizes.getTotalWidth() - 200) / 2, y: (GetSizes.getTotalHeight() - 200) / 2, width: 200, height: 200))
+            context.view.addSubview(self.loaderView)
+            self.loaderView.setFont(_font: font)
+            self.loaderView.setBackgroundColor(_color: color)
         }
         
         return loaderView
     }
-
+    
+    class func removeViewIfExist() -> LoadingDialogView? {
+        if (self.loaderView == nil) {
+            //do nothing here
+        } else {
+            self.loaderView.removeFromSuperview()
+        }
+        
+        return loaderView
+    }
     
     public class func show(context: UIViewController) {
-        LoadingDialog.sharedView(context: context)?.showLoader()
+        LoadingDialog.addView(context: context)?.showLoader()
     }
     
     public class func hide(context: UIViewController) {
-        LoadingDialog.sharedView(context: context)?.hideLoader()
+        LoadingDialog.removeViewIfExist()?.hideLoader()
     }
     
     public class func showLoader(context: UIViewController, withColor color: UIColor, andFont font: UIFont) {
-        LoadingDialog.sharedLoaderView(context: context, withColor: color, andFont: font)?.showLoader()
+        LoadingDialog.addView(context: context, withColor: color, andFont: font).showLoader()
     }
 }
